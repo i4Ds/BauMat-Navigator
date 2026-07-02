d0ced41b55e56f2d2edeba0fd3acdc68c57b94a1
Marko-5400 <marko.akrap07@gmail.com>
2026-07-02 13:21:39 +0200

    Feat: Add simple test to DataConverter

--- BauMat.MaterialDataConverter/Program.cs
+++ BauMat.MaterialDataConverter/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿if (args.Length < 2)
+{
+    Console.WriteLine("Usage: BauMat.MaterialDataConverter <inputFile> <outputFile>");
+    return;
+}
+
+var inputFile = args[0];
+var outputFile = args[1];
+
+Console.WriteLine("Input File: {inputFile}");
+Console.WriteLine("Output File: {outputFile}");
