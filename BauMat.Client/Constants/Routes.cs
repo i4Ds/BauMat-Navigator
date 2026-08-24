c3567f8f839aa60c3e3b7e078b1dd217e2f0f0dc
bernhardpages <linus-bernhard@outlook.com>
2026-08-24 15:50:26 +0200

    refactor: use segment-count logic instead of hardcoded route string

---
+++ BauMat.Client/Constants/Routes.cs
@@ -0,0 +1,12 @@
+namespace BauMat.Client.Constants;
+
+public static class Routes
+{
+    public const string Home = "/";
+    public const string Katalog = "/katalog";
+    public const string KatalogWithCategory = "/katalog/{category}";
+    public const string Material = "/material/{id:int}";
+    public const string Vergleich = "/vergleich";
+    public const string Methodik = "/methodik";
+    public const string NotFound = "/not-found";
+}
